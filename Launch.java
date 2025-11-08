af365766790ec980b52f2b327f1ab5f7f20ec539
JankiNandanC2 <jankinandan72@gmail.com>
2025-11-08 08:27:11 +0530

    modified after cloning

--- Launch.java
+++ Launch.java
@@ -1,6 +1,12 @@
 class Launch{
     public static void main(String[] args) {
-	System.out.println("Exploring Git and GitHub");
-	System.out.println("understanding Git and Github");
+        System.out.println("Exploring Git and GitHub");
+	    System.out.println("understanding Git and Github");
+    }
+    public static void fun(){
+        int a = 20 ;
+        int b = 30 ;
+        int sum = a + b ;
+        System.out.println(sum);
     }
 }
